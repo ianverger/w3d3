6b2a0abca57e0ed46274f8a354859173d3a7c9e9
Avisek Pandit <simonp4466@gmail.com>
2022-08-03 17:36:19 -0400

    attempt at merge_sort

--- recursion.rb
+++ recursion.rb
@@ -1,3 +1,5 @@
+
+
 def range(start, finish)
     return [] if finish <= start
     return [start] if finish == start + 1
@@ -95,10 +97,43 @@ def bsearch(arr, target)
     end
 end
 
-p bsearch([1, 2, 3], 1) # => 0
-p bsearch([2, 3, 4, 5], 3) # => 1
-p bsearch([2, 4, 6, 8, 10], 6) # => 2
-p bsearch([1, 3, 4, 5, 9], 5) # => 3
-p bsearch([1, 2, 3, 4, 5, 6], 6) # => 5
-p bsearch([1, 2, 3, 4, 5, 6], 0) # => nil
-p bsearch([1, 2, 3, 4, 5, 7], 6) # => nil
+# p bsearch([1, 2, 3], 1) # => 0
+# p bsearch([2, 3, 4, 5], 3) # => 1
+# p bsearch([2, 4, 6, 8, 10], 6) # => 2
+# p bsearch([1, 3, 4, 5, 9], 5) # => 3
+# p bsearch([1, 2, 3, 4, 5, 6], 6) # => 5
+# p bsearch([1, 2, 3, 4, 5, 6], 0) # => nil
+# p bsearch([1, 2, 3, 4, 5, 7], 6) # => nil
+
+def merge_sort(arr)
+    p arr
+    return [arr] if arr.length <= 1
+    middle = arr.length / 2
+
+    first_half = arr[0...middle]
+    second_half = arr[middle...arr.length]
+
+    # if arr.length == 2
+    #     first_half = [arr[0]]
+    #     second_half = [arr[1]]
+    # end
+
+    merge_sort(first_half) + merge_sort(second_half)
+end
+
+arr = [38,27,43,3,9,82,10]
+
+print merge_sort(arr)
+
+# def merge(arr)
+#     return arr if arr.flatten == arr
+    
+#     arr.each.with_index do |sub, i|
+#         if i % 2 == 0
+#             pair = [arr[i], arr[i + 1]]
+#             merge(pair)
+#         end
+#     end
+
+
+# end
