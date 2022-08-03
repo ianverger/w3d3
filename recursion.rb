34c33ccc8cfbc3750d31d1b587402d22e999c832
ianverger <ian.verger@gmail.com>
2022-08-03 16:28:02 -0400

    bsearch

--- recursion.rb
+++ recursion.rb
@@ -62,3 +62,43 @@ end
 # p arr2_dup = arr2.deep_dup
 
 # p arr2_dup.object_id == arr2.object_id
+
+def fib_iter(n)
+    seq = [0, 1, 1]
+    return seq.take(n) if n <= 3
+    (n - 3).times { seq << seq[-1] + seq[-2] }
+    seq
+end
+
+def fib_rec(n)
+    return [0, 1].take(n) if n <= 2
+    fib_rec(n - 1) << fib_rec(n - 1)[-1] + fib_rec(n - 1)[-2] 
+end
+
+def bsearch(arr, target)
+    return nil if arr.empty? 
+    middle = arr.length / 2
+    if target == arr[middle]
+        return middle 
+    elsif target > arr[middle] 
+        second = arr[middle + 1...arr.length]
+        second_cut = bsearch(second, target) 
+        if second_cut == nil 
+            return nil
+        else
+            return middle + 1 + second_cut
+        end
+        
+    else
+        first = arr[0...middle]
+        return bsearch(first, target)
+    end
+end
+
+p bsearch([1, 2, 3], 1) # => 0
+p bsearch([2, 3, 4, 5], 3) # => 1
+p bsearch([2, 4, 6, 8, 10], 6) # => 2
+p bsearch([1, 3, 4, 5, 9], 5) # => 3
+p bsearch([1, 2, 3, 4, 5, 6], 6) # => 5
+p bsearch([1, 2, 3, 4, 5, 6], 0) # => nil
+p bsearch([1, 2, 3, 4, 5, 7], 6) # => nil
